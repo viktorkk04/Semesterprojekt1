cf3e348da9477b555bb15e1d83505f03798d875f
Viktor Krolykke <viktorkrolykke@gmail.com>
2026-09-15 09:13:19 +0200

    add filer

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int y= 0;
+int x= 0;
+
+for (y = 1; y <= 3; y++){
+    Console.WriteLine("");
+    Console.Write("|");
+    //Console.Write(y*1);
+    for (x = 1; x <= 3; x++){
+        Console.Write("|");
+        Console.Write("-");
+        //Console.Write(x*1);
+        //Console.WriteLine(y*1);
+    }   
+    
+}
